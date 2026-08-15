0167dba03084f291c7c5500fec2b89f5de818093
ivolucas7 <ivolucas711@gmail.com>
2026-08-14 22:05:41 -0300

    Refactor input handling and loop for number printing
    Added input validation and modified loop structure.

--- atividade9.py
+++ atividade9.py
@@ -3,3 +3,20 @@ n2 = int(input("Digite um numero:"))
 
 for numero in range (n1 +1,n2):
     print(numero)
+
+
+
+
+
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:"))
+
+if n1>n2:
+    n1,n2=n2,n1
+
+    while n1<n2-1:
+        n1+=1
+        print(n1)
+
+#for numero in range (n1 +1,n2):
+   # print(numero)
