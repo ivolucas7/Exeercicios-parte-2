cadd346b6e22cadb4a7c2848a02d9ed4e5904e16
ivolucas7 <ivolucas711@gmail.com>
2026-08-21 21:51:39 -0300

    Update atividade11.py

--- atividade11.py
+++ atividade11.py
@@ -5,3 +5,22 @@ for i in range(5):
     numeros.append(numero)
 
 print(numeros)
+
+
+
+
+
+
+
+lista = []
+#roda 5 vezes para o usuario digitar os 5 numeros 
+for i in range(5):
+    numero= int(input(f"Digite o{1+1}° numero"))
+    lista.append(numero)
+
+
+print("os numeros inseridos da lista sao:")
+#Exibe cada item da lista
+for i in lista:
+    print(i)
+    
